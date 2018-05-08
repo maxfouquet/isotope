b466fce197af42befc041b620d6eb7d66f4a3156
Tyler Berry <tyler@berryac.com>
2018-05-08 10:59:04 -0700

    Add pkg/kubernetes

---
+++ pkg/kubernetes/config_map.go
@@ -0,0 +1,91 @@
+package kubernetes
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Tahler/service-grapher/pkg/graph"
+)
+
+type configMapService struct {
+	ComputeUsage float64       `json:"computeUsage"`
+	MemoryUsage  float64       `json:"memoryUsage"`
+	ErrorRate    float64       `json:"errorRate"`
+	Script       []interface{} `json:"script,omitempty"`
+}
+
+// MarshalYAML implements the Marshaler interface and converts a Service to a
+// marshallable interface{}.
+func serviceToMarshallable(
+	service graph.Service) (marshallable interface{}, err error) {
+	script, err := scriptToMarshallable(service.Script)
+	if err != nil {
+		return
+	}
+	marshallable = configMapService{
+		ComputeUsage: service.ComputeUsage,
+		MemoryUsage:  service.MemoryUsage,
+		ErrorRate:    service.ErrorRate,
+		Script:       script,
+	}
+	return
+}
+
+func scriptToMarshallable(script []graph.Executable) ([]interface{}, error) {
+	marshallableSlice := make([]interface{}, 0, len(script))
+	for _, exe := range script {
+		marshallable, err := executableToMarshallable(exe)
+		if err != nil {
+			return nil, err
+		}
+		marshallableSlice = append(marshallableSlice, marshallable)
+	}
+	return marshallableSlice, nil
+}
+
+func executableToMarshallable(
+	exe graph.Executable) (marshallable interface{}, err error) {
+	switch cmd := exe.(type) {
+	case graph.SleepCommand:
+		marshallable = sleepCommandToMarshallable(cmd)
+	case graph.RequestCommand:
+		marshallable = requestCommandToMarshallable(cmd)
+	case graph.ConcurrentCommand:
+		marshallable, err = concurrentCommandToMarshallable(cmd)
+	default:
+		err = fmt.Errorf("unexpected type %T", cmd)
+	}
+	return
+}
+
+func sleepCommandToMarshallable(cmd graph.SleepCommand) interface{} {
+	return map[string]string{"sleep": cmd.Duration.String()}
+}
+
+func requestCommandToMarshallable(cmd graph.RequestCommand) interface{} {
+	marshallable := make(map[string]configMapRequestCommand, 1)
+	key := strings.ToLower(string(cmd.HTTPMethod))
+	marshallable[key] = configMapRequestCommand{
+		Service:     cmd.ServiceName,
+		PayloadSize: cmd.PayloadSize,
+	}
+	return marshallable
+}
+
+type configMapRequestCommand struct {
+	Service     string `json:"service"`
+	PayloadSize int64  `json:"payloadSize"`
+}
+
+func concurrentCommandToMarshallable(
+	cmd graph.ConcurrentCommand) (interface{}, error) {
+	cmdList := make([]interface{}, 0, len(cmd.Commands))
+	for _, subCmd := range cmd.Commands {
+		marshallable, err := executableToMarshallable(subCmd)
+		if err != nil {
+			return nil, err
+		}
+		cmdList = append(cmdList, marshallable)
+	}
+	return cmdList, nil
+}
---
+++ pkg/kubernetes/kubernetes.go
@@ -0,0 +1,115 @@
+// Package kubernetes converts service graphs into Kubernetes manifests.
+package kubernetes
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/Tahler/service-grapher/pkg/graph"
+	"github.com/ghodss/yaml"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ServiceGraphToKubernetesManifests converts a ServiceGraph to Kubernetes
+// manifests.
+func ServiceGraphToKubernetesManifests(
+	serviceGraph graph.ServiceGraph) (yamlDoc []byte, err error) {
+	// ConfigMap manifest + 2 manifests per service (i.e. Deployment, Service).
+	numManifests := len(serviceGraph.Services)*2 + 1
+	manifests := make([]string, 0, numManifests)
+
+	appendManifest := func(manifest interface{}) error {
+		yamlDoc, err := yaml.Marshal(manifest)
+		if err != nil {
+			return err
+		}
+		manifests = append(manifests, string(yamlDoc))
+		return nil
+	}
+
+	configMap, err := makeConfigMap(serviceGraph)
+	if err != nil {
+		return
+	}
+	appendManifest(configMap)
+
+	for _, service := range serviceGraph.Services {
+		k8sDeployment, err := makeDeployment(service)
+		if err != nil {
+			return nil, err
+		}
+		appendManifest(k8sDeployment)
+
+		k8sService, err := makeService(service)
+		if err != nil {
+			return nil, err
+		}
+		appendManifest(k8sService)
+	}
+
+	yamlDocString := strings.Join(manifests, "---\n")
+	fmt.Printf("%+v\n", yamlDocString)
+	return []byte(yamlDocString), nil
+}
+
+func makeConfigMap(
+	graph graph.ServiceGraph) (configMap apiv1.ConfigMap, err error) {
+	configMap.APIVersion = "v1"
+	configMap.Kind = "ConfigMap"
+	configMap.ObjectMeta.Name = "scripts"
+	timestamp(&configMap.ObjectMeta)
+
+	data := make(map[string]string)
+	for _, service := range graph.Services {
+		marshallable, err := serviceToMarshallable(service)
+		if err != nil {
+			return configMap, err
+		}
+		serviceYAML, err := yaml.Marshal(marshallable)
+		if err != nil {
+			return configMap, err
+		}
+		data[service.Name] = string(serviceYAML)
+	}
+	configMap.Data = data
+	return
+}
+
+func makeService(service graph.Service) (k8sService apiv1.Service, err error) {
+	k8sService.APIVersion = "v1"
+	k8sService.Kind = "Service"
+	k8sService.ObjectMeta.Name = service.Name
+	timestamp(&k8sService.ObjectMeta)
+	return
+}
+
+const containerName = "performance-test"
+const containerImage = "istio.gcr.io/performance-test"
+
+func makeDeployment(
+	service graph.Service) (k8sDeployment appsv1.Deployment, err error) {
+	k8sDeployment.APIVersion = "apps/v1"
+	k8sDeployment.Kind = "Deployment"
+	k8sDeployment.ObjectMeta.Name = service.Name
+	timestamp(&k8sDeployment.ObjectMeta)
+	k8sDeployment.Spec = appsv1.DeploymentSpec{
+		Template: apiv1.PodTemplateSpec{
+			Spec: apiv1.PodSpec{
+				Containers: []apiv1.Container{
+					{
+						Name:  containerName,
+						Image: containerImage,
+					},
+				},
+			},
+		},
+	}
+	return
+}
+
+func timestamp(objectMeta *metav1.ObjectMeta) {
+	objectMeta.CreationTimestamp = metav1.Time{Time: time.Now()}
+}
