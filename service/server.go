cb6e91f7937967e60379aa7d762ac1538842076b
Tyler Berry <tyler@berryac.com>
2018-05-08 14:40:47 -0700

    Server handler based on graph.Service config

--- service/server.go
+++ service/server.go
@@ -4,18 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/Tahler/service-grapher/pkg/graph"
 )
 
 const port = 8080
 
-func echoHandler(writer http.ResponseWriter, request *http.Request) {
+func main() {
+	service := readService()
+	handler := serviceHandler{Service: service}
+	log.Printf("Listening on port %v\n", port)
+	addr := fmt.Sprintf(":%v", port)
+	http.ListenAndServe(addr, handler)
+}
+
+type serviceHandler struct {
+	graph.Service
+}
+
+func (h serviceHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	for _, cmd := range h.Script {
+		cmd.Execute()
+	}
 	log.Printf("Echoing %s to client %s", request.URL.Path, request.RemoteAddr)
 	request.Write(writer)
 }
 
-func main() {
-	http.HandleFunc("/", echoHandler)
-	log.Printf("Listening on port %v\n", port)
-	addr := fmt.Sprintf(":%v", port)
-	http.ListenAndServe(addr, nil)
+func readService() graph.Service {
+	return graph.Service{
+		ServiceSettings: graph.ServiceSettings{
+			ComputeUsage: 0.1,
+			MemoryUsage:  0.2,
+			ErrorRate:    0.5,
+		},
+		Name: "A",
+		Script: []graph.Executable{
+			graph.SleepCommand{Duration: 5 * time.Second},
+		},
+	}
 }
