b00c5131b94a24f1887685a309d22212048f73e3
Tyler Berry <tyler@berryac.com>
2018-05-14 14:35:21 -0700

    tmp: starts servers

--- service/server.go
+++ service/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/Tahler/service-grapher/pkg/graph"
+	"istio.io/fortio/fgrpc"
+	"istio.io/fortio/fhttp"
 )
 
-const port = 8080
+const port = "8080"
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -15,8 +19,42 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	handler := serviceHandler{Service: service}
-	log.Printf("Listening on port %v\n", port)
-	addr := fmt.Sprintf(":%v", port)
-	http.ListenAndServe(addr, handler)
+	err = startServer(service.Type)
+	if err != nil {
+		log.Fatal(err)
+	}
+	blockForever()
+}
+
+// TODO: This switch should instead be two different programs / images.
+func startServer(serviceType graph.ServiceType) (err error) {
+	switch serviceType {
+	case graph.HTTPService:
+		err = startHTTPServer()
+	case graph.GRPCService:
+		err = startGRPCServer()
+	default:
+		err = fmt.Errorf("unknown value of service type: %v", serviceType)
+	}
+	return
+}
+
+func startHTTPServer() (err error) {
+	_, tcpAddr := fhttp.Serve(port, "")
+	if tcpAddr == nil {
+		err = errors.New("failed to start HTTP server")
+	}
+	return
+}
+
+func startGRPCServer() (err error) {
+	boundPort := fgrpc.PingServer(port, fgrpc.DefaultHealthServiceName, 0)
+	if boundPort == -1 {
+		err = errors.New("failed to start GRPC server")
+	}
+	return
+}
+
+func blockForever() {
+	select {}
 }
