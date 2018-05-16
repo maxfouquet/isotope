71662e766c8b0288293dbd4f7a116f50671f31bc
Tyler Berry <tyler@berryac.com>
2018-05-15 22:48:24 -0700

    Add Service.MarshalJSON

---
+++ pkg/graph/svc/marshal_test.go
@@ -0,0 +1,40 @@
+package svc
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/Tahler/service-grapher/pkg/graph/svctype"
+)
+
+func TestService_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		input  Service
+		output []byte
+		err    error
+	}{
+		{
+			Service{
+				Name: "a",
+				Type: svctype.ServiceHTTP,
+			},
+			[]byte(`{"name":"a","type":"http"}`),
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			output, err := json.Marshal(test.input)
+			if test.err != err {
+				t.Errorf("expected %v; actual %v", test.err, err)
+			}
+			if string(test.output) != string(output) {
+				t.Errorf("expected %s; actual %s", test.output, output)
+			}
+		})
+	}
+}
--- pkg/graph/svc/service.go
+++ pkg/graph/svc/service.go
@@ -10,18 +10,18 @@ import (
 // Service describes a service in the service graph.
 type Service struct {
 	// Name is the DNS-addressable name of the service.
-	Name string
+	Name string `json:"name"`
 
 	// Type describes what protocol the service supports (e.g. HTTP, gRPC).
-	Type svctype.ServiceType
+	Type svctype.ServiceType `json:"type,omitempty"`
 
 	// ErrorRate is the percentage chance between 0 and 1 that this service
 	// should respond with a 500 server error rather than 200 OK.
-	ErrorRate pct.Percentage
+	ErrorRate pct.Percentage `json:"errorRate,omitempty"`
 
 	// ResponseSize is the number of bytes in the response body.
-	ResponseSize size.ByteSize
+	ResponseSize size.ByteSize `json:"responseSize,omitempty"`
 
 	// Script is sequentially called each time the service is called.
-	Script script.Script
+	Script script.Script `json:"script,omitempty"`
 }
