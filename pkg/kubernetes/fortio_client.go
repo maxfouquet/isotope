874e33c8770cd7464335c0a85fecd5692bdc65a1
Tyler Berry <tyler@berryac.com>
2018-06-08 23:33:26 +0000

    Add func ServiceGraphToFortioClientJobManifest

---
+++ pkg/kubernetes/fortio_client.go
@@ -0,0 +1,73 @@
+// Package kubernetes converts service graphs into Kubernetes manifests.
+package kubernetes
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Tahler/service-grapher/pkg/consts"
+	"github.com/Tahler/service-grapher/pkg/graph"
+	"github.com/Tahler/service-grapher/pkg/graph/svc"
+	"github.com/ghodss/yaml"
+	batchv1 "k8s.io/api/batch/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ServiceGraphToFortioClientManifest extracts the entrypoint into the service
+// graph and renders a Kubernetes Job manifest to run `Fortio load` on it.
+func ServiceGraphToFortioClientManifest(serviceGraph graph.ServiceGraph) (
+	manifest []byte, err error) {
+	entrypoints := make([]svc.Service, 0, 1)
+	for _, svc := range serviceGraph.Services {
+		if svc.IsEntrypoint {
+			entrypoints = append(entrypoints, svc)
+		}
+	}
+	numEntrypoints := len(entrypoints)
+	if numEntrypoints > 1 {
+		err = fmt.Errorf(
+			"cannot create client for service graph with multiple entrypoints: %v",
+			entrypoints)
+		return
+	}
+	if numEntrypoints < 1 {
+		err = errors.New(
+			"cannot create client for service graph with no entrypoints")
+		return
+	}
+	entrypoint := entrypoints[0]
+	job := entrypointToFortioClientJob(entrypoint)
+	manifestStr, err := yaml.Marshal(job)
+	if err != nil {
+		return
+	}
+	manifest = []byte(manifestStr)
+	return
+}
+
+const fortioImage = "istio/fortio:0.11.0"
+
+var fortioClientLabels = map[string]string{"app": "client"}
+
+func entrypointToFortioClientJob(entrypoint svc.Service) (job batchv1.Job) {
+	url := fmt.Sprintf("http://%s:%v", entrypoint.Name, consts.ServicePort)
+
+	job.APIVersion = "batch/v1"
+	job.Kind = "Job"
+	job.ObjectMeta.Name = "client"
+	job.Spec.Template = apiv1.PodTemplateSpec{
+		ObjectMeta: metav1.ObjectMeta{Labels: fortioClientLabels},
+		Spec: apiv1.PodSpec{
+			Containers: []apiv1.Container{
+				{
+					Name:  "fortio-client",
+					Image: fortioImage,
+					Args:  []string{"load", url},
+				},
+			},
+			RestartPolicy: apiv1.RestartPolicyNever,
+		},
+	}
+	return
+}
