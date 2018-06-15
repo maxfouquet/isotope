9fc5528935e4ece7e9f3a93a25ebf0675f33d697
Tyler Berry <tyler@berryac.com>
2018-06-15 11:45:33 -0700

    Configure logging level in args

--- runner/run_tests.py
+++ runner/run_tests.py
@@ -20,10 +20,9 @@ RETRY_INTERVAL = datetime.timedelta(seconds=5)
 
 
 def main() -> None:
-    logging.basicConfig(
-        level=logging.DEBUG, format='%(levelname)s\t> %(message)s')
-
     args = parse_args()
+    log_level = getattr(logging, args.log_level)
+    logging.basicConfig(level=log_level, format='%(levelname)s\t> %(message)s')
 
     for topology_path in args.topology_paths:
         service_graph_path, client_path = gen_yaml(topology_path)
@@ -41,6 +40,11 @@ def main() -> None:
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('topology_paths', metavar='PATH', type=str, nargs='+')
+    parser.add_argument(
+        '--log_level',
+        type=str,
+        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+        default='INFO')
     return parser.parse_args()
 
 
