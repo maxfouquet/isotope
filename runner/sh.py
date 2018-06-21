c8b9f13c3d479dea2a92b97ae98c255ca07e8944
Tyler Berry <tyler@berryac.com>
2018-06-21 15:55:21 -0700

    wip

--- runner/sh.py
+++ runner/sh.py
@@ -1,5 +1,6 @@
 import logging
 import subprocess
+import sys
 from typing import Dict, List
 
 
@@ -30,3 +31,28 @@ def run(args: List[str], check=False,
     if proc.stderr is not None:
         proc.stderr = proc.stderr.decode('utf-8').strip()
     return proc
+
+
+# def run(args: List[str], check=False,
+#         env: Dict[str, str] = None) -> subprocess.CompletedProcess:
+#     logging.debug('%s', args)
+#     try:
+#         proc = subprocess.Popen(
+#             # proc = subprocess.run(
+#             args,
+#             check=check,
+#             env=env,
+#             stdout=subprocess.PIPE)
+#         if logging.getLogger().isEnabledFor(logging.DEBUG):
+#             for line in iter(proc.stdout.readline, b''):
+#                 sys.stdout.write(line)
+
+#     except subprocess.CalledProcessError as e:
+#         logging.error('%s\n%s', e, e.stderr)
+#         raise e
+
+#     if proc.stdout is not None:
+#         proc.stdout = proc.stdout.decode('utf-8').strip()
+#     if proc.stderr is not None:
+#         proc.stderr = proc.stderr.decode('utf-8').strip()
+#     return proc
