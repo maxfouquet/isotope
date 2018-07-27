30e0455177b67fa06326cae255dba303795d056c
Tyler Berry <tyler@berryac.com>
2018-07-27 14:16:19 -0700

    Remove resource requests for mock-service

--- convert/pkg/kubernetes/kubernetes.go
+++ convert/pkg/kubernetes/kubernetes.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ghodss/yaml"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,9 +25,6 @@ const (
 
 	configVolume           = "config-volume"
 	serviceGraphConfigName = "service-graph-config"
-
-	serviceCPULimit    = "1"
-	serviceMemoryLimit = "3.75G"
 )
 
 var (
@@ -201,16 +197,6 @@ func makeDeployment(
 							fmt.Sprintf(
 								"--max-idle-connections-per-host=%v",
 								serviceMaxIdleConnectionsPerHost),
-						},
-						Resources: apiv1.ResourceRequirements{
-							Requests: apiv1.ResourceList{
-								apiv1.ResourceCPU:    resource.MustParse(serviceCPULimit),
-								apiv1.ResourceMemory: resource.MustParse(serviceMemoryLimit),
-							},
-							Limits: apiv1.ResourceList{
-								apiv1.ResourceCPU:    resource.MustParse(serviceCPULimit),
-								apiv1.ResourceMemory: resource.MustParse(serviceMemoryLimit),
-							},
 						},
 						Env: []apiv1.EnvVar{
 							{Name: consts.ServiceNameEnvKey, Value: service.Name},
