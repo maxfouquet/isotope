0a9cc7580139ed599f3fac70165aae8b0cae32ea
Max <fouquet.max@gmail.com>
2022-05-03 14:31:52 +0200

    Update kubernetes.go

--- convert/pkg/kubernetes/kubernetes.go
+++ convert/pkg/kubernetes/kubernetes.go
@@ -155,7 +155,7 @@ func makeService(service svc.Service) (k8sService apiv1.Service, err error) {
 	k8sService.ObjectMeta.Namespace = ServiceGraphNamespace
 	k8sService.ObjectMeta.Labels = serviceGraphAppLabels
 	timestamp(&k8sService.ObjectMeta)
-	k8sService.Spec.Ports = []apiv1.ServicePort{{Name: fmt.Sprintf("tcp-%s", consts.ServicePort), Port: consts.ServicePort}}
+	k8sService.Spec.Ports = []apiv1.ServicePort{{Name: fmt.Sprintf("tcp-%d", consts.ServicePort), Port: consts.ServicePort}}
 	k8sService.Spec.Selector = map[string]string{"name": service.Name}
 	return
 }
